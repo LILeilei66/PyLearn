846c78ed50ff208c086282d743a9a30e3e4f1d0e
LILeilei <40779351+LILeilei66@users.noreply.github.com>
2019-03-07 14:20:30 +0800

    Add files via upload

--- func_par_passing.py
+++ func_par_passing.py
@@ -23,14 +23,30 @@ e.g.,
 
 # http://winterttr.me/2015/10/24/python-passing-arguments-as-value-or-reference/
 """
+from copy import copy
 
 def change_int(a):
     a = 2
     return a
 
-def change_list(a):
+def change_list_element(a):
     a[0] = 2
     return a
+
+def change_value(a):
+    b = a
+    b[0] = 2
+    return b
+
+def change_list(a):
+    b = a
+    b = [2]
+    return b
+
+def change_copy_list(a):
+    b = copy(a)
+    b[0] = 2
+    return b
 
 if __name__ == '__main__':
     a = 1
@@ -38,5 +54,18 @@ if __name__ == '__main__':
     print('b: %d ; a : %d.' % (b, a)) # b: 2 ; a : 1.
 
     c = [1]
-    d = change_list(c)
+    d = change_list_element(c)
     print('c: {:} ; d : {:}.'.format(c, d)) # c: [2] ; d : [2].
+
+    e = [1]
+    f = change_value(e)
+    print('e: {:} ; f : {:}.'.format(e, f)) # e: [2] ; f : [2].
+
+    g = [1]
+    h = change_list(g)
+    print('g: {:} ; h : {:}.'.format(g, h)) # g: [1] ; h : [2].
+
+    i = [1]
+    j = change_copy_list(i)
+    print('i: {:} ; j : {:}.'.format(i, j)) # i: [1] ; j : [2].
+
