7046b9593c364422d2f1ccb1e248286334550c84
LILeilei <40779351+LILeilei66@users.noreply.github.com>
2019-04-03 16:50:49 +0800

    Add files via upload

--- int_type.py
+++ int_type.py
@@ -1,8 +1,12 @@
 """
 int type 会影响数据的加减.
 """
+import warnings
+warnings.filterwarnings('ignore')
 import numpy as np
 a = np.uint8(130)
 b = np.uint8(126)
 print(a + b, type(a + b)) # 0 <class 'numpy.uint8'>
+print(int(a) + int(b)) # 256
 print(b - a, type(b - a)) # 252 <class 'numpy.uint8'>
+print(int(b)-int(a)) # -4
