e841275b944d25b09ea3b40ca5492549b35e74e7
LILeilei <40779351+LILeilei66@users.noreply.github.com>
2019-05-08 13:53:25 +0800

    Add files via upload

--- functional_programming.py
+++ functional_programming.py
@@ -1,6 +1,8 @@
 """
 面对函数编程 与 面对对象编程
 """
+from functools import reduce
+
 square = lambda x: x * x
 for i in range(5):
     print(square(i))
@@ -8,6 +10,9 @@ print(list(map(square, list(range(5)))))
 
 odd = lambda x: x % 2
 print(list(filter(odd, list(range(5)))))
+
+customer_sum = lambda x, y: x + y
+print(reduce(customer_sum, list(range(5))))
 
 pr = lambda str : str
 num_pr = lambda x: ((x == 1) and pr('one')) \
